88694c0eef96b0f18162c12e4cf9979447779852
Rachael Thomas <rnthomas001@gmail.com>
2020-06-04 22:00:41 -0600

    Create lat lon list for cities and network requests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,55 @@ import React, { Component } from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import Landing from "../Landing/Landing";
+import { fetchWeather, fetchTrails } from "../../apiCalls.js";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      selectedCity: "",
+      selectedCity: "Arvada",
+      cityWeather: {},
+      trails: [],
     };
+  }
+
+  getCityLatLon = (city) => {
+    let lat;
+    let lon;
+    if (city === "Arvada") {
+      lat = 39.8028;
+      lon = -105.0875;
+    } else if (city === "Broomfield") {
+      lat = 39.9205;
+      lon = -105.0867;
+    } else if (city === "Lafayette") {
+      lat = 39.9936;
+      lon = -105.0897;
+    } else if (city === "Louisville") {
+      lat = 39.9778;
+      lon = -105.1319;
+    } else if (city === "Superior") {
+      lat = 39.9528;
+      lon = -105.1686;
+    } else if (city === "Wesminster") {
+      lat = 39.8397;
+      lon = -105.0372;
+    }
+    return [lat, lon];
+  };
+
+  componentDidMount() {
+    const [lat, lon] = this.getCityLatLon(this.state.selectedCity);
+    fetchWeather(lat, lon)
+      .then((data) => data)
+      .then((weather) => 
+      this.setState({ cityWeather: 
+        {temp: weather.main.temp,
+        type: weather.weather[0].main,
+        description: weather.weather[0].description} }));
+    fetchTrails(lat, lon)
+      .then((data) => data)
+      .then((trails) => this.setState({ cityTrails: trails }))
   }
 
   setSelectedCity = (city) => {
