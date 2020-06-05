4ca46d0907cc9220601c0e9c3f77f75aaf14fc06
Rachael Thomas <rnthomas001@gmail.com>
2020-06-04 19:09:19 -0600

    Create the functionality for a selected city to update state and to display an error message if no city is selected

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,26 +2,48 @@ import React, { Component } from "react";
 import "./Landing.css";
 
 class Landing extends Component {
-  constructor() {
+  constructor(props) {
     super();
-    this.state = {};
+    this.state = {
+      city: "",
+      errorMessage: "",
+    };
   }
+
+  handleSelection = (e) => {
+    this.setState({ city: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.state.city === ""
+      ? this.setState({ errorMessage: "You must select a city" })
+      : this.props.setSelectedCity(this.state.city);
+  };
 
   render() {
     return (
       <div className="landing-container">
         <form>
           <h2>Welcome!</h2>
-          <p>Please select a city:</p>
-          <select>
-            <option>Arvada</option>
-            <option>Broomfield</option>
-            <option>Lafayette</option>
-            <option>Louisville</option>
-            <option>Superior</option>
-            <option>Westminster</option>
+          <p>Please select a city you would like to run in:</p>
+          <select
+            className="city-selection"
+            placeholder="select a city"
+            onChange={this.handleSelection}
+          >
+            <option disabled selected value="select a city">Select a City</option>
+            <option value="Arvada">Arvada</option>
+            <option value="Broomfield">Broomfield</option>
+            <option value="Lafayette">Lafayette</option>
+            <option value="Louisville">Louisville</option>
+            <option value="Superior">Superior</option>
+            <option value="Westminster">Westminster</option>
           </select>
-          <button>Find Trails</button>
+          <button className="enter-page-button" onClick={this.handleSubmit}>
+            Find Trails
+          </button>
+          <p className="error-message">{this.state.errorMessage}</p>
         </form>
       </div>
     );
