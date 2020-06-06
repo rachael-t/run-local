a69de4097f18777b2645361aee6dff9929de1775
Rachael Thomas <rnthomas001@gmail.com>
2020-06-06 15:50:42 -0600

    Update image info to render and the pathname for viewing details

--- src/components/TrailCard/TrailCard.js
+++ src/components/TrailCard/TrailCard.js
@@ -3,17 +3,16 @@ import "./TrailCard.css";
 import { Link } from "react-router-dom";
 
 const TrailCard = (props) => {
-  console.log("hello");
   return (
     <div className="trail-card">
-      <h2>{props.name}</h2>
+      <h2>{props.trailInfo.name}</h2>
       <img
         className="trail-card-image"
-        src={props.image ? props.image : "/images/no-image-found.jpg"}
+        src={props.trailInfo.imgSmallMed ? props.trailInfo.imgSmallMed : "/images/no-image-found.jpg"}
         alt=""
       />
-      <Link to={`/`} className="trail-card-link">
-        <button className="view-trail-details">View Trail Details</button>
+      <Link to={`/trails/${props.city}/${props.trailInfo.name}`} className="trail-card-link">
+        <button className="view-trail-details" onClick={() => props.setSelectedTrail(props)}>View Trail Details</button>
       </Link>
     </div>
   );
