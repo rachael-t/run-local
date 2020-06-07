cc2200cddecd71d8ab646786d808eb21557250a7
Rachael Thomas <rnthomas001@gmail.com>
2020-06-07 14:12:53 -0600

    Clean up test file

--- src/components/Sidebar/Sidebar.test.js
+++ src/components/Sidebar/Sidebar.test.js
@@ -13,14 +13,12 @@ describe("Sidebar", () => {
       </MemoryRouter>
     );
 
-    const sidebarMessage = getByText(
-      "Select a different city:"
-    );
-    const citySelectDropdown = getByPlaceholderText('select a city');
+    const sidebarMessage = getByText("Select a different city:");
+    const citySelectDropdown = getByPlaceholderText("select a city");
     const newTrailsButton = getByRole("button", { name: "Find New Trails" });
-    const savedTrailsButton = getByRole("button", {name: "My Saved Trails"})
-    const storesMessage = getByText("Support your local running stores:")
-    const localStores = getAllByRole('listitem')
+    const savedTrailsButton = getByRole("button", { name: "My Saved Trails" });
+    const storesMessage = getByText("Support your local running stores:");
+    const localStores = getAllByRole("listitem");
 
     expect(sidebarMessage).toBeInTheDocument();
     expect(citySelectDropdown).toBeInTheDocument();
