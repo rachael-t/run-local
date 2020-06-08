617c67baa6ef54b7560f5f2121e215b16effc448
Rachael Thomas <rnthomas001@gmail.com>
2020-06-08 13:42:29 -0600

    Write unit test for BadPath component

---
+++ src/components/BadPath/BadPath.test.js
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom/";
+import { MemoryRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import BadPath from "./BadPath";
+import React from "react";
+
+describe("BadPath", () => {
+  it("should display the message and button to return home when rendered", () => {
+    const { getByText, getByRole } = render(
+      <MemoryRouter>
+        <BadPath />
+      </MemoryRouter>
+    );
+
+    const errorMessage = getByText(
+      "Hmm... doesn't look like there is a trail here. Time to turn around!"
+    );
+    const returnButton = getByRole("button", { name: "Return Home" });
+
+    expect(errorMessage).toBeInTheDocument();
+    expect(returnButton).toBeInTheDocument();
+  });
+});
