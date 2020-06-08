3be152124a3b2eca1a9303925395914ff0a473dd
Rachael Thomas <rnthomas001@gmail.com>
2020-06-08 10:37:21 -0600

    Remove mocked out data and add in a loading message to appear while data is being retrieved

--- src/components/LocationContainer/LocationContainer.js
+++ src/components/LocationContainer/LocationContainer.js
@@ -3,80 +3,12 @@ import "./LocationContainer.css";
 import { fetchWeather, fetchTrails } from "../../apiCalls.js";
 import TrailCard from "../TrailCard/TrailCard";
 
-// let cityWeather = {
-//   description: "overcast clouds",
-//   temp: 71.46,
-//   type: "Cloud",
-// };
-
-// let cityTrails = [
-//   {
-//     id: 7011192,
-//     name: "Boulder Skyline Traverse",
-//     summary: "The classic long mountain route in Boulder.",
-//     difficulty: "black",
-//     location: "Superior, Colorado",
-//     url:
-//       "https://www.trailrunproject.com/trail/7011192/boulder-skyline-traverse",
-//     imgSmall:
-//       "https://cdn2.apstatic.com/photos/hike/7048859_small_1555540136.jpg",
-//     imgSmallMed:
-//       "https://cdn2.apstatic.com/photos/hike/7048859_smallMed_1555540136.jpg",
-//     imgMedium:
-//       "https://cdn2.apstatic.com/photos/hike/7048859_medium_1555540136.jpg",
-//     length: 16.3,
-//     conditionStatus: "All Clear",
-//     conditionDetails: "Dry",
-//     conditionDate: "2020-05-27 14:38:34",
-//   },
-//   {
-//     id: 7000130,
-//     name: "Bear Peak Out and Back",
-//     summary: "A must-do run for Boulder locals and visitors alike!",
-//     difficulty: "black",
-//     location: "Boulder, Colorado",
-//     url: "https://www.trailrunproject.com/trail/7000130/bear-peak-out-and-back",
-//     imgSmall:
-//       "https://cdn2.apstatic.com/photos/hike/7005382_small_1554312030.jpg",
-//     imgSmallMed:
-//       "https://cdn2.apstatic.com/photos/hike/7005382_smallMed_1554312030.jpg",
-//     imgMedium:
-//       "https://cdn2.apstatic.com/photos/hike/7005382_medium_1554312030.jpg",
-//     length: 5.7,
-//     conditionStatus: "All Clear",
-//     conditionDetails: "Dry",
-//     conditionDate: "2020-05-22 16:05:31",
-//   },
-//   {
-//     id: 7004226,
-//     name: "Sunshine Lion's Lair Loop",
-//     summary:
-//       "Great Mount Sanitas views are the reward for this gentler loop in Sunshine Canyon.",
-//     difficulty: "blueBlack",
-//     location: "Boulder, Colorado",
-//     url:
-//       "https://www.trailrunproject.com/trail/7004226/sunshine-lions-lair-loop",
-//     imgSmall:
-//       "",
-//     imgSmallMed:
-//       "",
-//     imgMedium:
-//       "",
-//     length: 5.3,
-//     conditionStatus: "All Clear",
-//     conditionDetails: "Dry - Clear of ice and mud",
-//     conditionDate: "2020-05-08 14:06:04",
-//   },
-// ];
-
 class LocationContainer extends Component {
   constructor(props) {
     super();
     this.state = {
       cityWeather: "",
       cityTrails: [],
-      // cityWeather: cityWeather,
-      // cityTrails: cityTrails
     };
   }
 
@@ -154,12 +86,24 @@ class LocationContainer extends Component {
         />
       );
     });
+
+    const checkLoading = () => {
+      if (this.state.cityTrails.length === 0) {
+        return <p className="loading-message">Loading...</p>;
+      } else {
+        return (
+          <div>
+            <p>The current temperature is {this.state.cityWeather.temp}°F</p>
+            <p>Current conditions are: {this.state.cityWeather.description}</p>
+            <div className="trails-container">{cityTrailsToRender}</div>
+          </div>
+        );
+      }
+    };
     return (
       <div className="location-container">
         <h2>{this.props.match}</h2>
-        <p>The current temperature is {this.state.cityWeather.temp}°F</p>
-        <p>Current conditions are: {this.state.cityWeather.description}</p>
-        <div className="trails-container">{cityTrailsToRender}</div>
+        {checkLoading()}
       </div>
     );
   }
