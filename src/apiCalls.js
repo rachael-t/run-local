26310511f44b3691a7fb9d12c66efd6a1cbd5eeb
Rachael Thomas <rnthomas001@gmail.com>
2020-06-08 14:15:32 -0600

    Change to async function and add in an alert if an error occurs

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,19 @@
-export const fetchWeather = (lat, lon) => {
-  console.log("lat", lat);
-  console.log("lon", lon);
-  return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6d1ace92366d9a453c37cee4b46329fe&units=imperial`
-  ).then((response) => response.json());
+export const fetchWeather = async (lat, lon) => {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6d1ace92366d9a453c37cee4b46329fe&units=imperial`);
+    return await response.json();
+  }
+  catch (err) {
+    return alert(`We sincerely apologize. We encountered an error of: ${err}`);
+  }
 };
 
-export const fetchTrails = (lat, lon) => {
-  console.log("lat", lat);
-  console.log("lon", lon);
-  return fetch(
-    `https://www.trailrunproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=5&key=200784597-944ac7b994117cd50260ab05aae14a92`
-  ).then((response) => response.json());
+export const fetchTrails = async (lat, lon) => {
+  try {
+    const response = await fetch(`https://www.trailrunproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=5&key=200784597-944ac7b994117cd50260ab05aae14a92`);
+    return await response.json();
+  }
+  catch (err) {
+    return alert(`We sincerely apologize. We encountered an error of: ${err}`);
+  }
 };
